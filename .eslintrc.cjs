70d7854cd5a50b0610a7b765c6016f654ea3df5b
Sean Lailvaux <142589+seanlail@users.noreply.github.com>
2022-05-17 22:36:03 +0100

    Add eslint-plugin-import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,22 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	plugins: ['import', 'svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	rules: {
+		'import/newline-after-import': ['error', { count: 1 }],
+		'import/order': [
+			'warn',
+			{
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true
+				}
+			}
+		]
+	},
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
