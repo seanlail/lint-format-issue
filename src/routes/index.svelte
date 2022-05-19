792812aa4590bdca0febdfc324b1d44c1f0d25c6
Sean Lailvaux <142589+seanlail@users.noreply.github.com>
2022-05-19 12:56:26 +0100

    Update settings.json as suggested and save route
    Suggestion here: https://github.com/sveltejs/language-tools/issues/1486#issuecomment-1131217306

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
-	import RelativeA from './_RelativeA.svelte';
-	import RelativeB from './_RelativeB.svelte';
-
 	import ComponentA from '$component/ComponentA.svelte';
 	import ComponentB from '$component/ComponentB.svelte';
 	import { exampleStore } from '$component/stores';
+	import { onMount } from 'svelte';
+	import RelativeA from './_RelativeA.svelte';
+	import RelativeB from './_RelativeB.svelte';
 
 	onMount(() => {
 		// do nothing
